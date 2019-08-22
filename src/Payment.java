87ee5a9607f2714c8ce5d9cf2c86f7e64e5564a0
Aleh <yem1403@gmail.com>
2019-08-22 10:13:30 +0300

    Initial commit

---
+++ src/Payment.java
@@ -0,0 +1,63 @@
+/*Задача 2.
+Создать класс Payment с внутренним классом, с помощью объектов которого можно сформировать покупку
+из нескольких товаров.*/
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Payment {
+
+    private List<Goods> goods;
+
+    public Payment() {
+        goods = new ArrayList<>();
+    }
+
+    public void addGoods(Goods goods) {
+        this.goods.add(goods);
+    }
+
+    public void showBasket() {
+        int number = 1;
+        for (Goods item : goods) {
+            System.out.println(number + item.toString());
+            number++;
+        }
+    }
+
+    public class Goods {
+
+        private String name;
+        private Units units;
+        private double amount;
+        private double priceForUnit;
+
+        public Goods(String name, Units units, double amount, double priceForUnit) {
+            this.name = name;
+            this.units = units;
+            this.amount = amount;
+            this.priceForUnit = priceForUnit;
+        }
+
+        @Override
+        public String toString() {
+            return ". " + name + ", " + amount + " " + units.getUnits() + ". For payment: " + (priceForUnit * amount) + ".";
+        }
+    }
+
+    public enum Units {
+
+        KG("kg"), PIECE("pcs"), LITER("liter");
+
+        private String units;
+
+        Units(String units) {
+            this.units = units;
+        }
+
+        public String getUnits() {
+            return units;
+        }
+    }
+
+}
---
+++ src/PaymentMain.java
@@ -0,0 +1,17 @@
+/*Задача 2.
+Создать класс Payment с внутренним классом, с помощью объектов которого можно сформировать покупку
+из нескольких товаров.*/
+
+public class PaymentMain {
+
+    public static void main(String[] args) {
+        Payment payment = new Payment();
+        Payment.Goods bread = payment.new Goods("Bread", Payment.Units.PIECE, 2, 3.29);
+        Payment.Goods milk = payment.new Goods("Milk", Payment.Units.LITER, 1, 1.39);
+        Payment.Goods nuts = payment.new Goods("Nuts", Payment.Units.KG, 1.4, 0.99);
+        payment.addGoods(bread);
+        payment.addGoods(milk);
+        payment.addGoods(nuts);
+        payment.showBasket();
+    }
+}
